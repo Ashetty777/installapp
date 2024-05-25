e17a9ccfedca18f2fb5eb927cd9d265c6cf6d755
Ashetty777 <ashrithshetty260@gmail.com>
2024-05-25 09:51:14 +0530

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,64 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'My PWA',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      exclude: [/_redirects/], // Exclude redirects (if any)
+      runtimeCaching: [
+        {
+          urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images',
+            expiration: {
+              maxEntries: 60,
+              maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+            },
+          },
+        },
+        {
+          urlPattern: new RegExp('^https://your-api-domain.com/'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api',
+            networkTimeoutSeconds: 10,
+            expiration: {
+              maxEntries: 50,
+              maxAgeSeconds: 5 * 60, // 5 minutes
+            },
+          },
+        },
+        {
+          urlPattern: new RegExp('^https://fonts.(?:googleapis|gstatic).com/(.*)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'google-fonts',
+            expiration: {
+              maxEntries: 30,
+              maxAgeSeconds: 365 * 24 * 60 * 60, // 1 year
+            },
+          },
+        },
+        {
+          urlPattern: /.*/,
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'general',
+            networkTimeoutSeconds: 10,
+            expiration: {
+              maxEntries: 200,
+              maxAgeSeconds: 24 * 60 * 60, // 1 day
+            },
+          },
+        },
+      ],
+    },
+  },
 })
